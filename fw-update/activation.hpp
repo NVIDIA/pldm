NVIDIA/pldm

commit 98fd6f53e07400cd7a54c9de8f7b5715659b05c1

Fixes compilation process on a local machine. Removed redundant parameters from all constructors of classes that inherits from the sdbusplus object class.
Test:
Successfully compiled PLDM project on a local machine.

fw-update/activation.hpp
@@ -91,7 +91,7 @@ class Activation : public ActivationIntf
      */
     Activation(sdbusplus::bus::bus& bus, std::string objPath,
                Activations activationStatus, UpdateManager* updateManager) :
-        ActivationIntf(bus, objPath.c_str(), true),
+        ActivationIntf(bus, objPath.c_str()),
         bus(bus), objPath(objPath), updateManager(updateManager)
     {
         activation(activationStatus);

fw-update/device_inventory.cpp
@@ -7,7 +7,7 @@ namespace pldm::fw_update::device_inventory
 
 Entry::Entry(sdbusplus::bus::bus& bus, const pldm::dbus::ObjectPath& objPath,
              const pldm::UUID& mctpUUID, const Associations& assocs) :
-    Ifaces(bus, objPath.c_str(), true)
+    Ifaces(bus, objPath.c_str())
 {
     Ifaces::type(ChassisType::Component, true);
     Ifaces::uuid(mctpUUID, true);

fw-update/firmware_inventory.cpp
@@ -7,7 +7,7 @@ namespace pldm::fw_update::fw_inventory
 
 Entry::Entry(sdbusplus::bus::bus& bus, const std::string& objPath,
              const std::string& versionStr) :
-    Ifaces(bus, objPath.c_str(), true)
+    Ifaces(bus, objPath.c_str())
 {
     Ifaces::version(versionStr, true);
     Ifaces::purpose(VersionPurpose::Other, true);

pldmd/dbus_impl_pdr.hpp
@@ -40,7 +40,7 @@ class Pdr : public PdrIntf
      */
     Pdr(sdbusplus::bus::bus& bus, const std::string& path,
         const pldm_pdr* repo) :
-        PdrIntf(bus, path.c_str(), repo),
+        PdrIntf(bus, path.c_str()),
         pdrRepo(repo){};
 
     /** @brief Implementation for PdrIntf.FindStateEffecterPDR

commit 4422fff7943a38cb608c322b89ed740b81574692

Fix regression causing dbus object creation signal being fired before all properties are set.
Broken by commit 98fd6f53e07

Tested along with SPDM on lunadp-b1-14.
Verified that erot endpoints are properly created in SPDM tree (proves the signal is issued after all properties are set).
Did not test any other pldmd functionality (don't know how).

Fixes jira https://jirasw.nvidia.com/browse/DGXOPENBMC-4915

fw-update/activation.hpp
@@ -98,7 +98,7 @@ class Activation : public ActivationIntf
      */
     Activation(sdbusplus::bus::bus& bus, std::string objPath,
                Activations activationStatus, UpdateManager* updateManager) :
-        ActivationIntf(bus, objPath.c_str()),
+        ActivationIntf(bus, objPath.c_str(), action::defer_emit),
         bus(bus), objPath(objPath), updateManager(updateManager)
     {
         activation(activationStatus);

fw-update/device_inventory.cpp
@@ -7,7 +7,7 @@ namespace pldm::fw_update::device_inventory
 
 Entry::Entry(sdbusplus::bus::bus& bus, const pldm::dbus::ObjectPath& objPath,
              const pldm::UUID& mctpUUID, const Associations& assocs) :
-    Ifaces(bus, objPath.c_str())
+    Ifaces(bus, objPath.c_str(), action::defer_emit)
 {
     Ifaces::type(ChassisType::Component, true);
     Ifaces::uuid(mctpUUID, true);

fw-update/firmware_inventory.cpp
@@ -7,7 +7,7 @@ namespace pldm::fw_update::fw_inventory
 
 Entry::Entry(sdbusplus::bus::bus& bus, const std::string& objPath,
              const std::string& versionStr) :
-    Ifaces(bus, objPath.c_str())
+    Ifaces(bus, objPath.c_str(), action::defer_emit)
 {
     Ifaces::version(versionStr, true);
     Ifaces::purpose(VersionPurpose::Other, true);