0db03495817989200be2e24bdffea13b9b97f1a0
Gilbert Chen <gilbertc@nvidia.com>
2024-12-11 09:02:34 +0000

    platform-mc: skip to send setEventRecevier to non PLDM T2 terminus
    When the EID is back to online, pldmd should send "setTid" and
    "SetEventReceiver" commands to the EID again. the resume function
    should skip the EID which are not support PLDM T2.
    
    tested:
    Ensure the pldmd journallog has no log captured below during host
    power on/off.
    
    ```
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: Response not received for the request, instance ID expired. EID=26, INSTANCE_ID=1 ,TYPE=0, COMMAND=1
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: Response not received for the request, instance ID expired. EID=27, INSTANCE_ID=1 ,TYPE=0, COMMAND=1
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: No response received, EID=19
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: sendRecvPldmMsgOverMctp failed. eid=19 rc=1
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: setTidOverMctp failed. eid=19 tid=19 rc=1
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: No response received, EID=26
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: sendRecvPldmMsgOverMctp failed. eid=26 rc=1
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: setTidOverMctp failed. eid=26 tid=26 rc=1
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: No response received, EID=27
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: sendRecvPldmMsgOverMctp failed. eid=27 rc=1
    Dec 11 08:40:14 gb200nvl-hmc pldmd[12001]: setTidOverMctp failed. eid=27 tid=27 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: Response not received for the request, instance ID expired. EID=19, INSTANCE_ID=0 ,TYPE=0, COMMAND=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: Response not received for the request, instance ID expired. EID=27, INSTANCE_ID=0 ,TYPE=0, COMMAND=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: Response not received for the request, instance ID expired. EID=18, INSTANCE_ID=0 ,TYPE=0, COMMAND=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: Response not received for the request, instance ID expired. EID=26, INSTANCE_ID=0 ,TYPE=0, COMMAND=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: No response received, EID=19
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: sendRecvPldmMsgOverMctp failed. eid=19 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: setTidOverMctp failed. eid=19 tid=19 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: No response received, EID=27
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: sendRecvPldmMsgOverMctp failed. eid=27 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: setTidOverMctp failed. eid=27 tid=27 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: No response received, EID=18
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: sendRecvPldmMsgOverMctp failed. eid=18 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: setTidOverMctp failed. eid=18 tid=18 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: No response received, EID=26
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: sendRecvPldmMsgOverMctp failed. eid=26 rc=1
    Dec 11 08:40:29 gb200nvl-hmc pldmd[12001]: setTidOverMctp failed. eid=26 tid=26 rc=1
    ```
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/4983144
    
    Signed-off-by: Gilbert Chen <gilbertc@nvidia.com>
    
    Co-authored-by: Aditya Kurdunkar <akurdunkar@nvidia.com>

--- platform-mc/manager.hpp
+++ platform-mc/manager.hpp
@@ -83,7 +83,7 @@ class Manager : public pldm::MctpDiscoveryHandlerIntf
                             [[maybe_unused]] const EID& eid) override
     {
         auto terminus = terminusManager.getTerminus(uuid);
-        if (terminus)
+        if (terminus && terminus->doesSupport(PLDM_PLATFORM))
         {
             sensorManager.setOnline(terminus->getTid());
         }
@@ -93,7 +93,7 @@ class Manager : public pldm::MctpDiscoveryHandlerIntf
                              [[maybe_unused]] const EID& eid) override
     {
         auto terminus = terminusManager.getTerminus(uuid);
-        if (terminus)
+        if (terminus && terminus->doesSupport(PLDM_PLATFORM))
         {
             sensorManager.setOffline(terminus->getTid());
         }
@@ -225,6 +225,12 @@ class Manager : public pldm::MctpDiscoveryHandlerIntf
         if (it != termini.end())
         {
             auto& terminus = it->second;
+            if (!terminus->doesSupport(PLDM_PLATFORM))
+            {
+                terminus->resumed = true;
+                co_return PLDM_SUCCESS;
+            }
+
             auto rc = co_await terminusManager.resumeTid(tid);
             if (rc != PLDM_SUCCESS)
             {
