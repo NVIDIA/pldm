1009f490842c2f591cf7d04257f23e9b19b502fc
Mark Kuo <makuo@nvidia.com>
2025-02-18 22:57:37 -0800

    pldmtool: Support to parse PLDM Oem PDR
    Change:
    * Add the pldmtool to support PLDM_OEM_PDR command code.
    
    Tested:
    * Check the output of PDR parsing data has included the OEM data.
    ```
    {
        "nextRecordHandle": 193,
        "responseCount": 24,
        "recordHandle": 192,
        "PDRHeaderVersion": 1,
        "PDRType": "OEM PDR",
        "recordChangeNumber": 0,
        "dataLength": 14,
        "vendorDataLength": 6,
        "vendorSpecificData[1]": 3,
        "vendorSpecificData[2]": 87,
        "vendorSpecificData[3]": 1,
        "vendorSpecificData[4]": 3,
        "vendorSpecificData[5]": 3,
        "vendorSpecificData[6]": 8
    }
    ```
    
    Fixes nvbug https://nvbugspro.nvidia.com/bug/4958440
    
    Signed-off-by: Mark Kuo <makuo@nvidia.com>

--- pldmtool/pldm_platform_cmd.cpp
+++ pldmtool/pldm_platform_cmd.cpp
@@ -1087,6 +1087,31 @@ class GetPDR : public CommandInterface
         output["containerID"] = unsigned(pdr->container_id);
     }
 
+    void printPDROem(uint8_t* data, ordered_json& output)
+    {
+        if (data == NULL)
+        {
+            std::cerr << "Failed to get the PDR Oem" << std::endl;
+            return;
+        }
+
+        pldm_oem_pdr* pdr = reinterpret_cast<pldm_oem_pdr*>(data);
+        uint16_t len = pdr->data_length + 1;
+        uint8_t index = 1;
+        uint8_t* vdata = pdr->vendor_specific_data;
+
+        output["vendorDataLength"] = unsigned(pdr->data_length);
+        while (index < len)
+        {
+            std::string key =
+                "vendorSpecificData[" + std::to_string(index) + "]";
+            output[key] = unsigned(*vdata);
+
+            vdata++;
+            index++;
+        }
+    }
+
     void printPDREntityAssociation(uint8_t* data, ordered_json& output)
     {
         const std::map<uint8_t, const char*> assocationType = {
@@ -1502,6 +1527,9 @@ class GetPDR : public CommandInterface
                 break;
             case PLDM_PDR_FRU_RECORD_SET:
                 printPDRFruRecordSet(data, output);
+                break;
+            case PLDM_OEM_PDR:
+                printPDROem(data, output);
                 break;
             default:
                 break;
