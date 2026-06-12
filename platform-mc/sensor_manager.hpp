NVIDIA/pldm

commit 9560a2b9bcc34c3553aa1e716b8ec810bb3cc020

config: remove unused configuration
Remove a bunch of configuration options that are not used in the OpenBMC
GitHub organization.  A quick global GitHub search doesn't turn up any
users in any forks either.  Remove the unused configuration until such a
time that it is required by a real use case.

Change-Id: I33980481797ceba89cfd8939082aff8860817fd2
Signed-off-by: Brad Bishop <bradbish@qti.qualcomm.com>

platform-mc/sensor_manager.hpp
@@ -21,6 +21,8 @@ namespace platform_mc
 
 using namespace pldm::pdr;
 
+inline constexpr uint64_t SENSOR_POLLING_TIME = 249;
+
 /**
  * @brief SensorManager
  *

requester/handler.hpp
@@ -141,9 +141,8 @@ class Handler
     explicit Handler(
         PldmTransport* pldmTransport, sdeventplus::Event& event,
         pldm::InstanceIdDb& instanceIdDb, bool verbose,
-        std::chrono::seconds instanceIdExpiryInterval =
-            std::chrono::seconds(INSTANCE_ID_EXPIRATION_INTERVAL),
-        uint8_t numRetries = static_cast<uint8_t>(NUMBER_OF_REQUEST_RETRIES),
+        std::chrono::seconds instanceIdExpiryInterval = std::chrono::seconds(5),
+        uint8_t numRetries = 2,
         std::chrono::milliseconds responseTimeOut =
             std::chrono::milliseconds(RESPONSE_TIME_OUT)) :
         pldmTransport(pldmTransport), event(event), instanceIdDb(instanceIdDb),

common/types.hpp
@@ -77,6 +77,10 @@ constexpr uint8_t BmcMctpEid = 8;
  */
 #define PLDM_PLATFORM_DEFAULT_MESSAGE_BUFFER_SIZE 256
 
+inline constexpr uint16_t HEARTBEAT_TIMEOUT = 120;
+inline constexpr uint8_t TERMINUS_ID = 1;
+inline constexpr uint16_t TERMINUS_HANDLE = 1;
+
 namespace dbus
 {
 

fw-update/device_updater.hpp
@@ -334,7 +334,7 @@ class DeviceUpdater
      * command is received from the FD during component image transfer stage
      *
      */
-    static constexpr int updateTimeoutSeconds = UPDATE_TIMEOUT_SECONDS;
+    static constexpr int updateTimeoutSeconds = 60;
 
     /**
      * @brief Timer to handle RequestFirmwareData timeout(UA_T2)

fw-update/update_manager.cpp
@@ -183,6 +183,7 @@ void UpdateManager::processStream(std::istream& package, uintmax_t packageSize)
     const auto& fwDeviceIDRecords = parser->getFwDeviceIDRecords();
     const auto& compImageInfos = parser->getComponentImageInfos();
 
+    static constexpr uint32_t MAXIMUM_TRANSFER_SIZE = 4096;
     for (const auto& deviceUpdaterInfo : deviceUpdaterInfos)
     {
         const auto& fwDeviceIDRecord =

host-bmc/dbus_to_event_handler.cpp
@@ -1,5 +1,6 @@
 #include "dbus_to_event_handler.hpp"
 
+#include "common/types.hpp"
 #include "libpldmresponder/pdr.hpp"
 
 #include <phosphor-logging/lg2.hpp>

host-bmc/host_pdr_handler.cpp
@@ -1,5 +1,7 @@
 #include "host_pdr_handler.hpp"
 
+#include "common/types.hpp"
+
 #include <libpldm/fru.h>
 #ifdef OEM_IBM
 #include <libpldm/oem/ibm/fru.h>

libpldmresponder/base.cpp
@@ -1,5 +1,6 @@
 #include "base.hpp"
 
+#include "common/types.hpp"
 #include "common/utils.hpp"
 #include "libpldmresponder/pdr.hpp"
 

commit eee21d0c83b27545ab3622412e9da33ab8c86ce5

Fix pldm build flag regressions
Upstream sync commit 9560a2b9 broke many meson options. Their resultant
macros are not used. The upstream code (hard-coded values) are used
instead.

Fix the following meson options:
- sensor-polling-time
- instance-id-expiration-interval
- number-of-request-retries

Fixes nvbug https://nvbugspro.nvidia.com/bug/6284671
Fixes nvbug https://nvbugspro.nvidia.com/bug/6301277

Signed-off-by: Paul Wood <pawood@nvidia.com>

platform-mc/sensor_manager.hpp
@@ -58,7 +58,7 @@ class SensorPollingEnableIntf : public EnableIntf
     SensorManager& parent;
 };
 
-inline constexpr uint64_t kSensorPollingTimeMs = 249;
+inline constexpr uint64_t kSensorPollingTimeMs = SENSOR_POLLING_TIME;
 
 /**
  * @brief SensorManager

requester/handler.hpp
@@ -161,13 +161,13 @@ class Handler
      *  @param[in] numRetries - number of request retries
      *  @param[in] responseTimeOut - time to wait between each retry
      */
-    explicit Handler(
-        PldmTransport* pldmTransport, sdeventplus::Event& event,
-        pldm::InstanceIdDb& instanceIdDb, bool verbose,
-        std::chrono::seconds instanceIdExpiryInterval = std::chrono::seconds(5),
-        uint8_t numRetries = 2,
-        std::chrono::milliseconds responseTimeOut =
-            std::chrono::milliseconds(RESPONSE_TIME_OUT)) :
+    explicit Handler(PldmTransport* pldmTransport, sdeventplus::Event& event,
+                     pldm::InstanceIdDb& instanceIdDb, bool verbose,
+                     std::chrono::seconds instanceIdExpiryInterval =
+                         std::chrono::seconds(INSTANCE_ID_EXPIRATION_INTERVAL),
+                     uint8_t numRetries = NUMBER_OF_REQUEST_RETRIES,
+                     std::chrono::milliseconds responseTimeOut =
+                         std::chrono::milliseconds(RESPONSE_TIME_OUT)) :
         pldmTransport(pldmTransport), event(event), instanceIdDb(instanceIdDb),
         verbose(verbose), instanceIdExpiryInterval(instanceIdExpiryInterval),
         numRetries(numRetries), responseTimeOut(responseTimeOut)